e396044da85c69ff846418f0c7d5c83e7c9d8b10
matanhaz <48633383+matanhaz@users.noreply.github.com>
2021-09-20 11:09:07 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+p = find_packages()
 setup(
     name='thesis',
     version='0.1.0',
-    packages=find_packages(include=['thesis', 'thesis.*'])
+    packages=p
 )
