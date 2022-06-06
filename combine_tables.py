e33d1c01effd16faa4f30511be163fdc2508b372
matanhaz <48633383+matanhaz@users.noreply.github.com>
2022-06-06 19:54:47 +0300

    Update combine_tables.py

--- combine_tables.py
+++ combine_tables.py
@@ -2,64 +2,36 @@ import csv
 import os
 
 
-all_rows = []
 file_name = 'data_all_methods_combined.csv'
 projects_dir_path = 'projects'
-projects = os.listdir(projects_dir_path)
-first_project = True
+
+path1 = os.path.join('Experiments', 'Experiment_1', 'data', file_name)
+path2 = os.path.join('Experiments', 'Experiment_2', 'data', file_name)
+path3 = os.path.join('Experiments', 'Experiment_4', 'data', file_name)
+path4 = os.path.join('barinel', 'matrixes_details.csv')
 
 
-for project in projects:
-    if os.path.isdir(os.path.join(projects_dir_path,project)):
-        path = os.path.join(projects_dir_path, project, 'Experiments', 'Experiment_1', 'data', file_name)
-        with open(path) as outfile:
-            rows = list(csv.reader(outfile))
-        if first_project:
-            first_project = False
-        else:
-            rows = rows[1:]
+def combine(path, file_name):
+    first_project = True
+    all_rows = []
+    for project in os.listdir(projects_dir_path):
+        if os.path.isdir(os.path.join(projects_dir_path,project)):
+            with open(os.path.join(projects_dir_path, project,path)) as outfile:
+                rows = list(csv.reader(outfile))
+            if first_project:
+                first_project = False
+            else:
+                rows = rows[1:]
 
-        all_rows.extend(rows)
+            all_rows.extend(rows)
 
-with open(os.path.join(projects_dir_path, "data_all_projects_localization.csv"), 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(all_rows)
-
-all_rows = []
-first_project = True
-
-for project in projects:
-    if os.path.isdir(os.path.join(projects_dir_path,project)):
-        path = os.path.join(projects_dir_path, project, 'Experiments', 'Experiment_2', 'data', file_name)
-        with open(path) as outfile:
-            rows = list(csv.reader(outfile))
-        if first_project:
-            first_project = False
-        else:
-            rows = rows[1:]
-
-        all_rows.extend(rows)
-
-with open(os.path.join(projects_dir_path, "data_all_projects_diagnosis.csv"), 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(all_rows)
+    with open(os.path.join(projects_dir_path, file_name), 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(all_rows)
 
 
-all_rows = []
-first_project = True
+combine(path1, "data_all_projects_localization.csv")
+combine(path2, "data_all_projects_diagnosis.csv")
+combine(path3, "data_all_projects_diagnosis_files_sim.csv")
+combine(path4, "data_all_projects_matrixes_details.csv")
 
-for project in projects:
-    if os.path.isdir(os.path.join(projects_dir_path,project)):
-        path = os.path.join(projects_dir_path, project, 'Experiments', 'Experiment_4', 'data', file_name)
-        with open(path) as outfile:
-            rows = list(csv.reader(outfile))
-        if first_project:
-            first_project = False
-        else:
-            rows = rows[1:]
-
-        all_rows.extend(rows)
-
-with open(os.path.join(projects_dir_path, "data_all_projects_diagnosis_files_sim.csv"), 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(all_rows)
